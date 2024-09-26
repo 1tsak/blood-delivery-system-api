43c01b94dbed050e4300b1b18f08c58cbec60887
Aakash Jha <hey.a2v10@gmail.com>
2024-09-26 13:26:42 +0530

    fixed update

--- delivery/views.py
+++ delivery/views.py
@@ -40,6 +40,24 @@ class DeliveryViewSet(viewsets.ModelViewSet):
         # Only return deliveries assigned to the current user or unassigned deliveries
         return Delivery.objects.filter(Q(delivery_staff=self.request.user) | Q(delivery_staff__isnull=True))
 
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
+
+    def partial_update(self, request, *args, **kwargs):
+        kwargs['partial'] = True
+        return self.update(request, *args, **kwargs)
+
     @action(detail=True, methods=['POST'])
     def scan_qr(self, request, pk=None):
         delivery = self.get_object()
